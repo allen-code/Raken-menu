2f84446cd2a8f4f27eeecd39b6e047dd63069821
Allen <cliu403@gmail.com>
2018-07-03 08:41:39 +1200

    remove store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ProfileStore from './store'
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-    <App profile={new ProfileStore()} />
-, document.getElementById('root'));
-registerServiceWorker();
+    <App />
+, document.getElementById('root'))
+registerServiceWorker()
